e43ca36f5bdf0f66137e77552b874d2ca0b02bd0
Adrian ANDRE <aandre@nx-domain.lan>
2023-01-05 11:15:11 +0100

    test case

---
+++ src/test/java/StepDefinition.java
@@ -0,0 +1,81 @@
+import io.cucumber.java.en.Given;
+
+public class StepDefinition {
+
+
+    @Given("i am alive")
+    public void i_am_alive() {
+        System.out.println("I am alive!");
+    }
+    @Given("This \" works")
+    public void this_works_double_quote() {
+        System.out.println("I am alive \"");
+
+    }
+    @Given("This ' works")
+    public void this_works_simple_quote() {
+        System.out.println("I am alive '");
+
+    }
+
+    @Given("This ` works")
+    public void this_works_grave_accent() {
+        System.out.println("I am alive `");
+
+    }
+
+
+    @Given("This $ works")
+    public void this_$_works() {
+        System.out.println("I am alive $");
+
+    }
+
+    @Given("This & works")
+    public void this_works_ampersan() {
+        System.out.println("I am alive &");
+
+    }
+
+    @Given("This ; works")
+    public void this_works_semicolon() {
+        System.out.println("I am alive ;");
+
+    }
+
+    @Given("This | works")
+    public void this_works_pipe() {
+        System.out.println("I am alive |");
+
+    }
+
+    @Given("This < works")
+    public void this_works_openbracket() {
+        System.out.println("I am alive <");
+
+    }
+    @Given("This ^ works")
+    public void this_works_caret() {
+        System.out.println("I am alive ^");
+
+    }
+
+    @Given("This % works")
+    public void this_works_percent() {
+        System.out.println("I am alive %");
+
+    }
+    @Given("This \\{} works")
+    public void this_works_brace() {
+        System.out.println("I am alive {}");
+
+    }
+    @Given("This > works")
+    public void this_works_closebracket() {
+        System.out.println("I am alive >");
+
+    }
+
+
+}
+
---
+++ src/test/java/test.feature
@@ -0,0 +1,41 @@
+Feature: test
+
+  Scenario: Test ",Test ',Test `,Test $,Test &,Test ;,Test |,Test <,Test ^,Test %,Test {},Test >
+    Given i am alive
+
+
+  Scenario: Test "
+    Given This " works
+
+  Scenario: Test '
+    Given This ' works
+
+  Scenario: Test `
+    Given This ` works
+
+  Scenario: Test $
+    Given This $ works
+
+  Scenario: Test &
+    Given This & works
+
+  Scenario: Test ;
+    Given This ; works
+
+  Scenario: Test |
+    Given This | works
+
+  Scenario: Test <
+    Given This < works
+
+  Scenario: Test ^
+    Given This ^ works
+
+  Scenario: Test %
+    Given This % works
+
+  Scenario: Test {}
+    Given This {} works
+
+  Scenario: Test >
+    Given This > works
